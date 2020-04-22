e1129f7405ab43ba3d2ba2d04ac40e389be6278f
bouracha <bouracha@tcd.ie>
2020-04-22 13:49:31 +0100

    Update psnr.py
    Added project argument

--- PSNR/psnr.py
+++ PSNR/psnr.py
@@ -7,24 +7,21 @@ name = sys.argv[0]
 epoch_num = sys.argv[1]
 data_set = sys.argv[2]
 num_images = sys.argv[3]
-
-print(name)
-print(epoch_num)
-print(num_images)
+project = sys.argv[4]
 
 PSNRs = []
 for i in range(1, int(num_images)+1):
   print(str(i) + "/" + str(num_images), end="\r")
-  #print("training_set/test_A/{0}.jpg".format(i))
-  #print("results/vinci/test_latest/images/{0}_synthesized_image.jpg".format(i))
   img1 = cv2.imread(str(data_set)+"/test_B/{0}.jpg".format(i))
-  img2 = cv2.imread("results/vinci/test_latest/images/{0}_synthesized_image.jpg".format(i))
+  img2 = cv2.imread("results/"+str(project)+"/test_latest/images/{0}_synthesized_image.jpg".format(i))
   assert(img1.shape == (1024, 1024, 3))
   assert(img2.shape == (1024, 1024, 3))
 
   PSNRs.append(cv2.PSNR(img1, img2))
 
-print("Epoch: ", epoch_num, np.mean(PSNRs), np.std(PSNRs))
+print("Data Set: ", data_set)
+print("Epoch: ", epoch_num)
+print("PSNR: ", np.mean(PSNRs), "+-", np.std(PSNRs))
 fields=[epoch_num, np.mean(PSNRs), np.std(PSNRs)]
 with open(r'PSNR/'+str(data_set)+'.csv', 'a') as f:
   writer = csv.writer(f)
