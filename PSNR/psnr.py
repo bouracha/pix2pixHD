fa0a9546b532acc9d7b29c17622cd1325a08f7f1
bouracha <bouracha@tcd.ie>
2020-04-22 15:37:49 +0100

    Update psnr.py
    Corrected datasets directory call

--- PSNR/psnr.py
+++ PSNR/psnr.py
@@ -12,13 +12,15 @@ project = sys.argv[4]
 PSNRs = []
 for i in range(1, int(num_images)+1):
   print(str(i) + "/" + str(num_images), end="\r")
-  img1 = cv2.imread(str(data_set)+"/test_B/{0}.jpg".format(i))
+  img1 = cv2.imread("datasets/"+str(data_set)+"/test_B/{0}.jpg".format(i))
   img2 = cv2.imread("results/"+str(project)+"/test_latest/images/{0}_synthesized_image.jpg".format(i))
   assert(img1.shape == (1024, 1024, 3))
   assert(img2.shape == (1024, 1024, 3))
 
   PSNRs.append(cv2.PSNR(img1, img2))
 
+with open('PSNR/'+str(data_set)+'.csv', "w") as my_empty_csv:
+  pass
 print("Data Set: ", data_set)
 print("Epoch: ", epoch_num)
 print("PSNR: ", np.mean(PSNRs), "+-", np.std(PSNRs))
