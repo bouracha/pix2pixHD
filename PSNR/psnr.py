6515d03db3651a20eeaa98de34790e8fc008d529
bouracha <bouracha@tcd.ie>
2020-05-24 15:35:26 +0100

    Changed to work with any name

--- PSNR/psnr.py
+++ PSNR/psnr.py
@@ -17,8 +17,8 @@ for i in image_list:
   print(str(i) + "/" + str(num_images), end="\r")
   img1 = IMAGE(path_to_image="datasets/" + str(data_set) + "/test_B", name_of_image=str(i))
   name = i[:-4]
-  img1 = IMAGE(path_to_image="results/" + str(project) + "/test_latest/images",
-               name_of_image="{0}_synthesized_image.jpg".format(name))
+  img2 = IMAGE(path_to_image="results/" + str(project) + "/test_latest/images",
+               name_of_image=str(name)+"_synthesized_image.jpg")
   if (img1.valid_image == False) or (img2.valid_image == False):
     continue
   assert(img1.shape() == img2.shape())
